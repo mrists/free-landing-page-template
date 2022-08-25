ceb5fea8d830cea0e1e7bbb9f2a234996e63c6cb
mrists <kalashnikoff.wh@gmail.com>
2022-08-25 14:27:44 +0300

    project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,5 @@
-const body = document.body
-const hamburgerBtn = document.querySelector('.header-navbar__hamburger');
-const navbarList = document.querySelector('.header-navbar__list');
+import Navbar from "./navbar.js"
 
-let isShow = false
+const navbar = new Navbar();
 
-function toggleActive() {
-  !isShow ? body.classList.toggle('active') : body.removeAttribute('class');
-  
-  hamburgerBtn.classList.toggle('active');
-  navbarList.classList.toggle('active');
-
-  isShow = !isShow;
-}
-
-hamburgerBtn.addEventListener('click', toggleActive);
+navbar.init();
